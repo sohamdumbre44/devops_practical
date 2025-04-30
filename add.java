d921de7b4b494f1b11001b7df569dc1df42497e2
sohamdumbre44 <soh.dum.rt22@dypatil.edu>
2025-04-30 11:15:25 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
+        int num1 = 55;
+        int num2 = 8;
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
